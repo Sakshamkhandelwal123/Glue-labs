cd0714442fa90ccd095e0751410a8e0f039b48cf
Saksham Khandelwal <sakshamkhandelwal2001@gmail.com>
2022-04-13 18:27:38 +0530

    Day 18

---
+++ hooks/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import HookCounter6 from './components/HookCounter6';
+// import ClassCounter2 from './components/ClassCounter2';
+// import HookCounter5 from './components/HookCounter5';
+// import ClassCounter1 from './components/ClassCounter1';
+// import HookCounter4 from './components/HookCounter4';
+// import HookCounter3 from './components/HookCounter3';
+// import ClassCounter from './components/ClassCounter';
+// import HookCounter from './components/HookCounter';
+// import HookCounter2 from './components/HookCounter2';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      {/* <HookCounter /> */}
+      {/* <HookCounter2 /> */}
+      {/* <HookCounter3 /> */}
+      {/* <HookCounter4 /> */}
+      {/* <ClassCounter1 /> */}
+      {/* <HookCounter5 /> */}
+      {/* <ClassCounter2 /> */}
+      <HookCounter6 />
+    </div>
+  );
+}
+
+export default App;
