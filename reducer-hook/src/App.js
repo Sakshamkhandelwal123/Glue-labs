66c6cd199a912463c952cfcd88caec00bf40c0c2
Saksham Khandelwal <sakshamkhandelwal2001@gmail.com>
2022-04-24 13:40:14 +0530

    Day 24

---
+++ reducer-hook/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import CounterOne from './components/CounterOne';
+// import CounterTwo from './components/CounterTwo';
+import DataFetching from './components/DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CounterOne /> */}
+      <CounterTwo />
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
